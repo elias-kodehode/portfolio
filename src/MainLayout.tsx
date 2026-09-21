086ebb4a267bb2c32788ab1f17058e1c2dfe8226
elias-kodehode <elias96.kodehode@gmail.com>
2026-09-21 14:29:39 +0200

    .

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -1,5 +1,17 @@
 import { useEffect, useRef, useState } from "react";
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./components/ui/button";
+import { Link } from "lucide-react";
+import { useTheme } from "./components/theme-provider";
+import { Menu } from "lucide-react";
 export default function MainLayout({
   children,
 }: {
@@ -53,17 +65,68 @@ function NavBar({ scrolled }: { scrolled: boolean }) {
       ].join(" ")}
     >
       <a href="/" className="font-heading text-xl font-semibold">
-        Portfolio
+        Elias Sørensen
       </a>
 
       <div className="flex items-center gap-2">
-        <a
-          href="/"
-          className="rounded-full px-4 py-2 text-sm text-muted-foreground transition hover:bg-accent hover:text-accent-foreground"
-        >
+        <HamburgerDropdown/>
+        {/* <a href="/" className="rounded-full px-4 py-2 text-sm text-muted-foreground transition hover:bg-accent hover:text-accent-foreground">
           Home
-        </a>
+        </a> */}
       </div>
     </nav>
+  );
+}
+
+
+function HamburgerDropdown() {
+  const { setTheme } = useTheme();
+
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger>
+        <Button
+          variant="outline"
+          size="icon"
+          className="fixed right-4 top-4 z-50 rounded-none rounded-2xl"
+        >
+          <Menu />
+          <span className="sr-only">Open menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="rounded-none">
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>My Socials</DropdownMenuLabel>
+
+          <DropdownMenuItem>
+          <a href="https://github.com/elias-kodehode" target="_blank"rel="noopener noreferrer">
+          Github</a>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <a href="">LinkedIn</a>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>Theme</DropdownMenuLabel>
+
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            Light
+          </DropdownMenuItem>
+
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
+            Dark
+          </DropdownMenuItem>
+
+          <DropdownMenuItem onClick={() => setTheme("system")}>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
